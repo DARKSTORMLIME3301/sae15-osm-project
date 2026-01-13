845cfb24fd42e9491178de2c515f9df79f00fad0
DARKSTORMLIME3301 <husseinabdulrahman968@gmail.com>
2026-01-13 11:50:00 +0100

    Remove unnecessary blank line and ensure consistent script termination message in md_to_html.py

--- python/md_to_html.py
+++ python/md_to_html.py
@@ -1,5 +1,4 @@
 import markdown
-
 def convert(md_file, html_file):
     """
     Convertit un fichier Markdown en fichier HTML.
@@ -21,3 +20,5 @@ def convert(md_file, html_file):
 # autorise de lancer le programme automatiquement
 if __name__ == "__main__":
     convert("../md/parks_sfax.md", "../html/parks_sfax.html")
+    print(">>> SCRIPT md_to_html.py TERMINÉ")
+    
